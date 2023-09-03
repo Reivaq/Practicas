0c06ed0595e1128a074d1980ca18b8564c4fc219
Reivaq <140466947+Reivaq@users.noreply.github.com>
2023-09-03 17:37:48 -0600

    Add files via upload

---
+++ First_exercice/part0/script.js
@@ -0,0 +1,54 @@
+/*console.log("Hola mundo")  // Logeamos un mensaje en la consola 
+const firstname=" Antonio"
+const firstnameWithUpercate =  firstname.toUpperCase() // cambiamos todas las letras a mayusculas 
+console.log(firstname)*/
+
+/*
+// Acceder ala posicion de un array 
+const list[]// crreamos una lista 
+list.push(157) // añanadimos un elemeto a nuestra lista 
+const anotherList = list.concat () // de esta forma podemos concatenar arrays, cadenas , nuevo s elementos a un array que querramos modificar 
+
+console.log(list[0])
+// Iteracion de una lista 
+anotherList.foreach()
+//Transformarmar una lista 
+anotherList.map 
+*/
+
+//creacion de objetos 
+
+const  datos {
+
+      name = 'antonio',
+      surname = ' Reivaj',
+      juegos = [ 'Gta', 'fornite', 'Mario', 'COD']
+
+}
+console.log(datos.name) // Des esta manera recuperamos los los datos que deseamos 
+console.log(datos.juegos[0])  // Accedemos a el dato de de juegos el primer valor de nuestro array 
+
+// Acceder a una variable especifica de datos 
+
+const field = 'surname'
+console.log(datos[field])
+
+//Funciones 
+
+// Definimos nuestra funcion 
+const sumar =(  A, B ) => {
+    console.log(A)
+    console.log(B)
+    return  A+B
+}
+
+//
+ console.log (sumar (3+3))  // muestra el resultado 
+
+
+// Declarar una funcion 
+
+function  restar (a,b){
+
+    return  A-B
+}
